552d021ddd43d7f349d285783cf7481f3ba0c6c6
ashaleyalcantara-cloud <ashaleyalcantara@gmail.com>
2026-09-09 10:55:02 -0300

    fix: corrige classe Guerreiro

--- Guerreiro.java
+++ Guerreiro.java
@@ -1,23 +1,26 @@
-public class Guerreiro implements Atacante { 
+public class Guerreiro extends Personagem implements Atacante {
 
     private int forca;
-    
- 
-    public void setForca(int forca){
-       if(forca >= 0){
-           this.forca = forca;
-       } else{
-          System.out.println("O valor da forca é inválido")
-       }
+
+    public Guerreiro(String nome, int nivel, int vida, int forca) {
+        super(nome, nivel, vida);
+        setForca(forca);
     }
- 
-    public int getForca(){
+
+    public int getForca() {
         return forca;
     }
- 
- 
-    public String Atacar(){
-       
+
+    public void setForca(int forca) {
+        if (forca >= 0) {
+            this.forca = forca;
+        } else {
+            System.out.println("O valor da força é inválido.");
+        }
     }
- 
- }
+
+    @Override
+    public String atacar() {
+        return "O guerreiro " + getNome() + " atacou com força " + forca + ".";
+    }
+}
