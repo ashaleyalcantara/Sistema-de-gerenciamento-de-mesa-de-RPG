11298c711356ce304b787e8e3b1c36661e6f3875
ashaleyalcantara-cloud <ashaleyalcantara@gmail.com>
2026-09-09 10:55:12 -0300

    fix: corrige classe Arqueiro

--- Arqueiro.java
+++ Arqueiro.java
@@ -1,23 +1,26 @@
-public class Arqueiro implements Atacante { 
+public class Arqueiro extends Personagem implements Atacante {
 
-   private int precisao;
-   
+    private int precisao;
 
-   public void setPrecisao(int precisao){
-      if(precisao >= 0){
-          this.precisao = precisao;
-      } else{
-         System.out.println("O valor da precisão é inválido")
-      }
-   }
+    public Arqueiro(String nome, int nivel, int vida, int precisao) {
+        super(nome, nivel, vida);
+        setPrecisao(precisao);
+    }
 
-   public int getPrecisao(){
-       return precisao;
-   }
+    public int getPrecisao() {
+        return precisao;
+    }
 
+    public void setPrecisao(int precisao) {
+        if (precisao >= 0) {
+            this.precisao = precisao;
+        } else {
+            System.out.println("O valor da precisão é inválido.");
+        }
+    }
 
-   public String Atacar(){
-      
-   }
-
+    @Override
+    public String atacar() {
+        return "O arqueiro " + getNome() + " atacou com precisão " + precisao + ".";
+    }
 }
