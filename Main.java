3ad6e97316d286ff87cb6cc7d6d35aea4d3e5917
ashaleyalcantara-cloud <ashaleyalcantara@gmail.com>
2026-09-10 20:19:29 -0300

    test: adiciona testes de participacao

--- Main.java
+++ Main.java
@@ -31,8 +31,11 @@ public class Main {
         jogador1.adicionarPersonagem(mago);
         jogador2.adicionarPersonagem(arqueiro);
 
-        Participacao part1 = new Participacao("Combatente", LocalDate.now());
-        Participacao part2 = new Participacao("Suporte", LocalDate.now());
+        Participacao part1 = new Participacao(jogador1, "Combatente", LocalDate.now());
+        Participacao part2 = new Participacao(jogador2, "Suporte", LocalDate.now());
+
+        jogador1.adicionarParticipacao(part1);
+        jogador2.adicionarParticipacao(part2);
 
         mesa.adicionarParticipacao(part1);
         mesa.adicionarParticipacao(part2);
@@ -45,22 +48,50 @@ public class Main {
         System.out.println(mago.atacar());
         System.out.println(arqueiro.atacar());
 
-        System.out.println("Mentor de " + guerreiro.getNome() + ": " + (guerreiro.getMentor() != null ? guerreiro.getMentor().getNome() : "Nenhum"));
+        System.out.println("Mentor de " + guerreiro.getNome() + ": " +
+                (guerreiro.getMentor() != null ? guerreiro.getMentor().getNome() : "Nenhum"));
 
         guerreiro.receberDano(30);
         System.out.println("Vida de " + guerreiro.getNome() + " após dano: " + guerreiro.getVida());
+
         guerreiro.curar(10);
         System.out.println("Vida de " + guerreiro.getNome() + " após cura: " + guerreiro.getVida());
 
         System.out.println("Itens de " + guerreiro.getNome() + " antes: " + guerreiro.listarItens().size());
+
         guerreiro.removerItem(escudo.getId());
+
         System.out.println("Itens de " + guerreiro.getNome() + " após remoção: " + guerreiro.listarItens().size());
 
-        System.out.println("Busca por ID 2: " + (jogador1.buscarPersonagem(2) != null ? jogador1.buscarPersonagem(2).getNome() : "Não encontrado"));
+        System.out.println("Busca por ID 2: " +
+                (jogador1.buscarPersonagem(2) != null ?
+                        jogador1.buscarPersonagem(2).getNome() : "Não encontrado"));
 
-        System.out.println("Total de personagens de " + jogador1.getNome() + " antes: " + jogador1.listarPersonagens().size());
+        System.out.println("Total de personagens de " + jogador1.getNome() + " antes: " +
+                jogador1.listarPersonagens().size());
+
         jogador1.removerPersonagem(mago.getId());
-        System.out.println("Total de personagens de " + jogador1.getNome() + " após remoção: " + jogador1.listarPersonagens().size());
+
+        System.out.println("Total de personagens de " + jogador1.getNome() + " após: " +
+                jogador1.listarPersonagens().size());
+
+        System.out.println("Participações de " + jogador1.getNome() + ": " +
+                jogador1.listarParticipacoes().size());
+
+        System.out.println("Jogador da participação 1: " +
+                part1.getJogador().getNome());
+
+        System.out.println("Papel da participação 1: " +
+                part1.getPapel());
+
+        System.out.println("Participações de " + jogador2.getNome() + ": " +
+                jogador2.listarParticipacoes().size());
+
+        System.out.println("Jogador da participação 2: " +
+                part2.getJogador().getNome());
+
+        System.out.println("Papel da participação 2: " +
+                part2.getPapel());
 
         System.out.println("Jogadores na mesa: " + mesa.listarJogadores().size());
         System.out.println("Participações registradas: " + mesa.listarParticipacoes().size());
